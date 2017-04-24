5869a98cbba15abac32584d362f41750acd9e1cd
BouncyJeffer <BouncyJeffer@gmail.com>
2017-04-23 18:52:37 -0700

    Created main.php
    Started the kickall command.

---
+++ src/ToolsPlus/main.php
@@ -0,0 +1,35 @@
+<?php
+namespace ServerTools;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\utils\TextFormat;
+class main extends PluginBase {
+	public function onLoad(){
+		$this->getLogger()->info(TextFormat::AQUA."currently being loaded.");
+	}
+	public function onEnable(){
+		$this->getLogger()->info(TextFormat::GREEN."has been successfully loaded.");
+	}
+	public function onDisable(){
+		$this->getLogger()->info(TextFormat::RED."has been disabled.");
+	}
+	public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+		switch($command->getName()){
+			case "kickall":
+			if( isset($args[0])){
+				$players = $this->getServer()->getOnlinePlayers();
+				foreach($players as $p){
+					if($p !== $sender){
+						$p->kick(implode(" ", $args));
+					}
+				} else {
+					$reason = "Unknown.";
+				}
+			}
+			break;
+		}
+	}
+}
